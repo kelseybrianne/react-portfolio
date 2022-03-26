03de95a5f0d3724b3017d7d3de3d7b778c5db7b1
Kelsey Mailau <kelseymailau@gmail.com>
2022-03-25 17:30:14 -0700

    add projproject screenshots to portfolio page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,10 @@ import resume from "../kelsey-mailau-resume.pdf";
 import portrait from "../portrait-of-kelsey-mailau.JPG";
 import { Container, Row, Col } from "react-bootstrap";
 import techBlog from "../technicals-homepage.png";
+import dayPlanner from "../day-planner-screenshot.png";
+import portfolio from "../portfolio-screenshot.png";
+import textEditor from "../text-editor-homepage.png";
+import codeQuiz from "../code-quiz-screenshot.png";
 
 const Navigation = ({ currentPage }) => {
   return currentPage === "About Me" ? (
@@ -10,25 +14,43 @@ const Navigation = ({ currentPage }) => {
       <h1>Explore my most recent projects in web development</h1>
       <Container className="portfolio-container">
         <Row>
-          <Col xs={12} md={6} lg={4}>
+          <Col xs={12} md={6} lg={4} className="relative">
             <img
               src={techBlog}
               className="portfolio-pic"
-              alt="Tech Blog homepage"
+              alt="tech blog homepage"
+            />
+            <div className="project-description">
+              <h3>Tech Blog</h3>
+              <p>This is a tech blog for techs yayyy</p>
+            </div>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <img
+              src={portfolio}
+              className="portfolio-pic"
+              alt="Portfolio homepage"
             />
           </Col>
           <Col xs={12} md={6} lg={4}>
             <img
-              src={techBlog}
+              src={dayPlanner}
               className="portfolio-pic"
-              alt="Tech Blog homepage"
+              alt="day planner homepage"
             />
           </Col>
           <Col xs={12} md={6} lg={4}>
             <img
-              src={techBlog}
+              src={textEditor}
               className="portfolio-pic"
-              alt="Tech Blog homepage"
+              alt="text editor homepage"
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <img
+              src={codeQuiz}
+              className="portfolio-pic"
+              alt="code quiz homepage"
             />
           </Col>
         </Row>
